8d62d8261aa51f9e8db61b278f2d744c61f2388e
Arnav Balpande <arnavbalpande14@gmail.com>
2024-02-22 19:51:59 +0530

    added homework fibo and fact

---
+++ factorial.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
+}
+func main() {
+	fmt.Println("Enter number to find factorial of")
+	var n int
+	fmt.Scan(&n)
+	fmt.Printf("Factorial of %d is %d", n, fact(n))
+}
